deb7b323a4b7785b9be0a91997d7696465b050ba
Dhom Abdi <dhom@gmail.com>
2023-08-27 18:39:47 +0300

    version 2

--- hello.c
+++ hello.c
@@ -2,4 +2,5 @@
 
 int main(){
     printf("hello world\n");
+    printf("this is version 2");
 }
--- src_folder/index.cpp
+++ src_folder/index.cpp
@@ -3,4 +3,5 @@ using namespace std;
 
 int main(){
     cout << "hello, I Don't know how to use Git.\n"; 
+    cout << "this is my Version 2\n";
 }
